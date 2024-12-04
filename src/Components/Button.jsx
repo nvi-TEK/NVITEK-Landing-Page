ec3e1a2f9977fec405813fe63b23258ed46e9693
kwekuduahh <asamoahduah101@gmail.com>
2024-12-04 21:18:39 +0000

    updated the website components

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,17 +1,26 @@
-import ButtonSvg from "../assets/svg/ButtonSvg.jsx";
+import ButtonSvg from "../assets/svg/ButtonSvg";
 
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const spanClasses = "relative z-10";
 
-const Button = ({className, href, children, white, px }) => {
-    const classes = `button relative inline-flex items-center justify-center h-11 w-32 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'}`;
+  const renderButton = () => (
+    <button className={classes} onClick={onClick}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
 
-    const renderButton = () =>(
-        <button className={classes}>
-            <span>{children}</span>
-            {ButtonSvg()}
-        </button>
-    )
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
 
-    return renderButton();
+  return href ? renderLink() : renderButton();
 };
 
 export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 import plugin from "tailwindcss/plugin";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./public/assets/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -73,13 +69,7 @@ export default {
       backgroundImage: {
         "radial-gradient": "radial-gradient(var(--tw-gradient-stops))",
         "conic-gradient":
-            "conic-gradient(from 225deg, #FFC876, #79FFF7, #9F53FF, #FF98E2, #FFC876)",
-        "benefit-card-1": "url(assets/benefits/card-1.svg)",
-        "benefit-card-2": "url(assets/benefits/card-2.svg)",
-        "benefit-card-3": "url(assets/benefits/card-3.svg)",
-        "benefit-card-4": "url(assets/benefits/card-4.svg)",
-        "benefit-card-5": "url(assets/benefits/card-5.svg)",
-        "benefit-card-6": "url(assets/benefits/card-6.svg)",
+          "conic-gradient(from 225deg, #FFC876, #79FFF7, #9F53FF, #FF98E2, #FFC876)",
       },
     },
   },
@@ -89,15 +79,15 @@ export default {
       addComponents({
         ".container": {
           "@apply max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem]":
-              {},
+            {},
         },
         ".h1": {
           "@apply font-semibold text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[3.25rem] lg:leading-[4.0625rem] xl:text-[3.75rem] xl:leading-[4.5rem]":
-              {},
+            {},
         },
         ".h2": {
           "@apply text-[1.75rem] leading-[2.5rem] md:text-[2rem] md:leading-[2.5rem] lg:text-[2.5rem] lg:leading-[3.5rem] xl:text-[3rem] xl:leading-tight":
-              {},
+            {},
         },
         ".h3": {
           "@apply text-[2rem] leading-normal md:text-[2.5rem]": {},
@@ -113,7 +103,7 @@ export default {
         },
         ".body-1": {
           "@apply text-[0.875rem] leading-[1.5rem] md:text-[1rem] md:leading-[1.75rem] lg:text-[1.25rem] lg:leading-8":
-              {},
+            {},
         },
         ".body-2": {
           "@apply font-light text-[0.875rem] leading-6 md:text-base": {},
@@ -123,7 +113,7 @@ export default {
         },
         ".tagline": {
           "@apply font-grotesk font-light text-xs tracking-tagline uppercase":
-              {},
+            {},
         },
         ".quote": {
           "@apply font-code text-lg leading-normal": {},
